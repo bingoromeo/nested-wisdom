fbeaf40654ee256c9d67892ab0d2672c20bef581
bingoromeo <bingoromeo1@gmail.com>
2025-07-06 00:02:04 +0000

    fix: updated astro config, tailwind setup, and dependencies

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,8 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 import netlify from '@astrojs/netlify';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  markdown: {
-    shikiConfig: {
-      theme: 'github-light-high-contrast',
-    },
-  },
-  integrations: [tailwind()],
   adapter: netlify(),
+  integrations: [tailwind()],
 });
