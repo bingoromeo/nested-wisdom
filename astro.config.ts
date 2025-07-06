9ad15c2f6d2aa5e4e4a6d054eb6a9596df9a1559
bingoromeo <bingoromeo1@gmail.com>
2025-07-06 01:39:26 +0000

    Update astro.config.ts for serverless Netlify deploy

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import netlify from '@astrojs/netlify/functions';
+import netlify from '@astrojs/netlify/functions'; // using serverless Netlify adapter
 
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: netlify(),
-});
+  adapter: netlify({}), // <-- pass an empty config object
+})
