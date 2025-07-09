4f79061b4540feabeaf3273eb9e49a36017044cd
bingoromeo <bingoromeo1@gmail.com>
2025-07-09 13:56:40 -0400

    Create speak.cjs

---
+++ netlify/functions/speak.cjs
@@ -0,0 +1,20 @@
+    }
+
+    const audioBuffer = await response.arrayBuffer();
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "audio/mpeg",
+        "Access-Control-Allow-Origin": "https://www.nestedwisdom.com"
+      },
+      body: Buffer.from(audioBuffer).toString("base64"),
+      isBase64Encoded: true
+    };
+  } catch (err) {
+    console.error("Speak error:", err);
+    return {
+      statusCode: 500,
+      body: "Voice generation failed"
+    };
+  }
+};
